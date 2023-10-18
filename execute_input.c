ce9d28f7a1fbe9c37abb8de5b5f014ad757ca9cd
ASLawan <nawalnitsua@gmail.com>
2023-10-18 08:06:21 +0100

    Parse and execute user input and input from stream

---
+++ execute_input.c
@@ -0,0 +1,46 @@
+#include "shell.h"
+/**
+ * execute_input - executes cammands
+ * @av: arguments array
+ * @usr_input: user command
+ * Return: av
+ */
+char **execute_input(char **av, char *usr_input)
+{
+	pid_t PID;
+	char *inpt = NULL, *inpt_to_execute = NULL, **arr = NULL;
+	int status, flag;
+
+	arr = parse_input(av, usr_input);
+	inpt = arr[0];
+	flag = input_check(arr, inpt);
+	if (flag != 1)
+	{
+		inpt_to_execute = get_input_path(inpt);
+		if (inpt_to_execute)
+		{
+			PID = fork();
+			if (PID == -1)
+			{
+				perror("Error");
+			}
+			else if (PID == 0)
+			{
+				if (execve(inpt_to_execute, arr, environ) == -1)
+				{
+					perror(inpt);
+				}
+			}
+			else
+			{
+				wait(&status);
+			}
+		}
+		else
+		{
+			perror(inpt);
+		}
+	}
+	free(arr);
+	return (arr);
+}
