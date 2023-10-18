7d8049191ed3cefdba3f6907876a2dd8ace0e82c
ASLawan <nawalnitsua@gmail.com>
2023-10-18 08:07:34 +0100

    Handle interactive and non-interactive modes

---
+++ interactive.c
@@ -0,0 +1,39 @@
+#include "shell.h"
+
+/**
+ * display_interactive - displays prompt
+ * @av: arguments array
+ * Return: 0, always (success)
+ */
+
+int display_interactive(char **av)
+{
+	char *usr_input = NULL; 
+	char *bye = "Exiting shell...";
+
+	while (true)
+	{
+		/*display prompt*/
+		display_iprompt();
+
+		/*read user input*/
+		usr_input = get_usr_input();
+		if (usr_input == NULL)
+		{
+			_printstr("./hsh: ");
+			_printstr(bye);
+			_printstr("\n");
+			free(usr_input);
+			return (-1);
+		}
+
+		/*parse and execute user input*/
+		execute_input(av, usr_input);
+		
+		free(usr_input);
+	}
+
+	free(usr_input);
+	free(bye);
+	return (0);
+}
