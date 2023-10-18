f81f8de12efab5d0b61f110fff8b423d87a0680f
qwab17 <quadriwasiubiodun@gmail.com>
2023-10-17 23:57:10 -0700

    non prompt file

---
+++ shell_non_iprompt.c
@@ -0,0 +1,14 @@
+#include "shell.h"
+
+
+/**
+ * display_non_iprompt - displays shell prompt
+ * Return: nothing
+ */
+
+void display_non_iprompt(void)
+{
+	char *prompt = "$ ";
+
+	_printstr(promp);
+}
