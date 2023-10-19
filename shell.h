bae735636ae2d65672f25a2193d75d50878bda4a
qwab17 <quadriwasiubiodun@gmail.com>
2023-10-19 03:09:58 -0700

    shell update

--- shell.h
+++ shell.h
@@ -13,10 +13,15 @@
 #include <sys/stat.h>
 #include <stdarg.h>
 
+void display_non_interactive(char **av);
 int main(void);
 void biggy_print(const char *format, ...);
 void d_prompt(void);
 void exec_cmd(const char *cmd, ...);
 void read_cmd(char *cmd, size_t size);
+char *get_input(void);
+char **execute_input(char **av, char *input);
+void display_non_iprompt(void);
+
 
 #endif
