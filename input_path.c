89dc81bd85e9a380c808f4aac1c9955e1206bef6
qwab17 <quadriwasiubiodun@gmail.com>
2023-10-19 00:42:25 -0700

    input path

--- input_path.c
+++ input_path.c
@@ -1,4 +1,5 @@
 #include "shell.h"
+
 /**
  * get_input_path - searches for command path
  * @input: command to search for
@@ -7,54 +8,38 @@
 char *get_input_path(char *input)
 {
 	struct stat st;
-	char *inpt_path = NULL, *file_path = NULL, *inpt_path_cpy = NULL;
-	char *tkn, *delim = ":";
-	int inpt_len, path_tkn_len;
+	char *inpt_path = _getenv("PATH");
+	char *inpt_path_cpy, *file_path, *tkn;
+	int inpt_len = _strlen(input), path_tkn_len;
 
-	inpt_path = _getenv("PATH");
-	if (inpt_path)
-	{
-		inpt_path_cpy = malloc(sizeof(char) * (_strlen(inpt_path) + 1));
-		if (inpt_path_cpy == NULL)
-		{
-			free(inpt_path_cpy);
-			perror("Error");
-			return (NULL);
-		}
-		_strcpy(inpt_path_cpy, inpt_path);
-		inpt_len = _strlen(input);
-		tkn = _strtok(inpt_path_cpy, delim);
-		while (tkn != NULL)
-		{
-			path_tkn_len = _strlen(tkn);
-			file_path = malloc(sizeof(char) * (inpt_len + path_tkn_len + 2));
-			if (file_path == NULL)
-			{
-				free(file_path);
-				perror("Error");
-				return (NULL);
-			}
-			_strcpy(file_path, tkn);
-			_strcat(file_path, "/");
-			_strcat(file_path, input);
-			_strcat(file_path, "\0");
-			if (stat(file_path, &st) == 0)
-			{
-				free(inpt_path_cpy);
-				return (file_path);
-			}
-			else
-			{
-				free(file_path);
-				tkn = _strtok(NULL, delim);
-			}
-		}
-		if (stat(input, &st) == 0)
-		{
-			free(inpt_path_cpy);
-			return (input);
-		}
+	if (inpt_path == NULL)
 		return (NULL);
+
+	inpt_path_cpy = _strdup(inpt_path);
+	if (inpt_path_cpy == NULL)
+		return (NULL);
+
+	tkn = _strtok(inpt_path_cpy, ":");
+	while (tkn)
+	{
+		path_tkn_len = _strlen(tkn);
+		file_path = malloc(inpt_len + path_tkn_len + 2);
+		if (file_path == NULL)
+			return (free(inpt_path_cpy), NULL);
+
+		_strcpy(file_path, tkn);
+		_strcat(file_path, "/");
+		_strcat(file_path, input);
+		if (stat(file_path, &st) == 0)
+			return (free(inpt_path_cpy), file_path);
+
+		free(file_path);
+		tkn = _strtok(NULL, ":");
 	}
+
+	if (stat(input, &st) == 0)
+		return (free(inpt_path_cpy), input);
+
+	free(inpt_path_cpy);
 	return (NULL);
 }
