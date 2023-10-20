b58959993459e454d5483994d0bf8a3a0ca54e06
ASLawan <nawalnitsua@gmail.com>
2023-10-21 00:58:30 +0100

    Implement env

--- input_check.c
+++ input_check.c
@@ -1,5 +1,29 @@
 #include "shell.h"
+void _printenv(void);
 int input_check(char **arr, char *inpt, char *usr_input);
+/**
+ * _printenv - prints environment variables
+ * Return: nothing
+ */
+void _printenv(void)
+{
+	int i;
+	char *value;
+	char *envs[] = {
+		"LESSOPEN", "LANGUAGE", "HOSTNAME", "SHLVL", "HOME",
+		"OLDPWD", "TERM", "PATH", "LANG", "LS_COLORS",
+		"NODE_PATH", "LESSCLOSE", "PWD", "LC_ALL", "TZ",
+		"HBTN", NULL};
+
+	for (i = 0; envs[i]; i++)
+	{
+		value = _getenv(envs[i]);
+		if (value)
+		{
+			printf("%s=%s\n", envs[i], value);
+		}
+	}
+}
 /**
  * input_check - executes cammands
  * @inpt: executable
@@ -43,5 +67,10 @@ int input_check(char **arr, char *inpt, char *usr_input)
 		_shell_exit(arr, usr_input);
 	if (strncmp(inpt, dir, 2) == 0)
 		change_dir(arr[1]);
+	if (strncmp(inpt, "env", 3) == 0)
+	{
+		_printenv();
+		flag = 1;
+	}
 	return (flag);
 }
